cd05e4d68dfae402d76cd11934c33bc08779d998
Lu Kotova <loui.kotova@gmail.com>
2023-03-13 15:39:34 +0100

    hw2_summary

--- src/main/java/java_summary/hw4/Task2.java
+++ src/main/java/java_summary/hw4/Task2.java
@@ -16,4 +16,34 @@ Input: nums = [1]
 Output: 1*/
 
 public class Task2 {
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1};
+        appearNonDuplicate(nums);
+        int[] nums1 = {4, 1, 2, 1, 2};
+        appearNonDuplicate(nums1);
+        int[] nums2 = {1};
+        appearNonDuplicate(nums2);
+        int[] nums3 = {1, 2, 3};
+        appearNonDuplicate(nums3);
+
+
+    }
+
+    static void appearNonDuplicate(int[] nums) {
+        for (int num : nums) {
+            int count = 0;
+            for (int element : nums) {
+                if (element == num) {
+                    count++;
+                    if (count > 1) {
+                        break;
+                    }
+                }
+            }
+            if (count == 1) {
+                System.out.println(num);
+                break;
+            }
+        }
+    }
 }
