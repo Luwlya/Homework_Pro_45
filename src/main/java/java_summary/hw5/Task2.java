e006e30b5854a1ab9947ade46a613ecd1a936f4e
Lu Kotova <loui.kotova@gmail.com>
2023-03-22 22:20:57 +0100

    hw5_summary

--- src/main/java/java_summary/hw5/Task2.java
+++ src/main/java/java_summary/hw5/Task2.java
@@ -22,5 +22,34 @@ class TestHashSet {
 }*/
 
 
-public class Task2 {
+import java.util.HashSet;
+import java.util.Objects;
+
+class Person {
+    String name;
+
+    Person(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
 }
+
+class TestHashSet {
+    public static void main(String args[]) {
+        HashSet set = new HashSet<>();
+        Person p1 = new Person("Иван");
+        Person p2 = new Person("Мария");
+        Person p3 = new Person("Пётр");
+        Person p4 = new Person("Мария");
+        set.add(p1);
+        set.add(p2);
+        set.add(p3);
+        set.add(p4);
+        System.out.print(set.size());
+    }
+
+}
+//выведет 4, потому что у класса Person не определен метод equals и hashCode. Поэтому p2 не равен p4.
